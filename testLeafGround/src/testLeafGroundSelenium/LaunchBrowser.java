fafb12364062360b46254b21650504ecb5e5c045
Shyam Sundar <shyam30042000@gmail.com>
2022-05-14 11:41:05 +0530

    First Commit

---
+++ testLeafGround/src/testLeafGroundSelenium/LaunchBrowser.java
@@ -0,0 +1,35 @@
+package testLeafGroundSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// Configuring the system properties of chrome driver
+		System.setProperty("webdriver.chrome.driver", "/home/shyam/chromedriver/chromedriver");
+		
+		//Initializing the browser driver
+		WebDriver driver = new ChromeDriver();
+		
+		//Navigating through a Google.co.in
+		driver.get("https://www.google.co.in");
+		
+		//Initializing SearchBox WebElement to store
+		WebElement SearchBox = driver.findElement(By.name("q"));
+		
+		//Search for Virat Kohli
+		SearchBox.sendKeys("Virat Kohli"+Keys.ENTER);
+		
+		//Entering Wikipedia of Virat
+		driver.findElement(By.linkText("Wikipedia")).click();
+		
+		//Closing the browser
+		driver.close();
+		
+	}
+
+}
