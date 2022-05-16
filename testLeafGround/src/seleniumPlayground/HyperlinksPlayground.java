96157d48a9baf4f15b70bd031d668e6d8573cf93
Shyam Sundar <shyam30042000@gmail.com>
2022-05-16 16:42:44 +0530

    HP Updated

--- testLeafGround/src/seleniumPlayground/HyperlinksPlayground.java
+++ testLeafGround/src/seleniumPlayground/HyperlinksPlayground.java
@@ -22,45 +22,42 @@ public class HyperlinksPlayground {
 
 		// Navigating through a Leaf Hyperlink Playground
 		driver.get("http://www.leafground.com/pages/Link.html");
-		
+
 		// Click on Home Page link using linkText
 		driver.findElement(By.linkText("Go to Home Page")).click();
-		
-		//Navigating back
-		driver.navigate().back(); 
-				
-		//Find where This link supposed to go without clicking
+
+		// Navigating back
+		driver.navigate().back();
+
+		// Find where This link supposed to go without clicking
 		String dest = driver.findElement(By.partialLinkText("without clicking")).getAttribute("href");
-		System.out.println("Link: Find where am supposed to go without clicking me? goes to "+dest);
-				
-		//Initializing HttpURLConnection and Verifying the page is broken or not
+		System.out.println("Link: Find where am supposed to go without clicking me? goes to " + dest);
+
+		// Initializing HttpURLConnection and Verifying the page is broken or not
 		HttpURLConnection huc = null;
 		WebElement brokenLink = driver.findElement(By.linkText("Verify am I broken?"));
 		String url = brokenLink.getAttribute("href");
 		try {
-			huc = (HttpURLConnection)(new URL(url).openConnection());
+			huc = (HttpURLConnection) (new URL(url).openConnection());
 			huc.setRequestMethod("HEAD");
 			huc.connect();
 			int respCode = huc.getResponseCode();
-			if(respCode >= 400){
+			if (respCode >= 400) {
 				System.out.println("The link is broken");
-			}
-			else{
+			} else {
 				System.out.println("The link valid");
 			}
-		}
-		catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		// Click on Home Page link using XPath
 		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div/div/a")).click();
-		//Navigating back
-		driver.navigate().back(); 
-		
-		
+		// Navigating back
+		driver.navigate().back();
+
 		List<WebElement> links = driver.findElements(By.tagName("a"));
-		System.out.println(links.size()+" links in this page");
+		System.out.println(links.size() + " links in this page");
 	}
 
 }
